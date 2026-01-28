52e493c66cd8cbc1c028050feb0338a26d5eded2
BahauddinRafiuddin <bahauddinrafiuddin@gmail.com>
2026-01-28 12:54:20 +0530

    Added validation and improved UI for login and register pages

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,102 +1,9 @@
-// import { useNavigate } from "react-router-dom";
-// import useAuth from "../../hooks/useAuth";
-// import { useState } from "react";
-// import { loginUser } from "../../api/auth.api";
-// import { toastError, toastSuccess } from "../../utils/toast";
-// const Login = () => {
-//   const { login } = useAuth();
-//   const navigate = useNavigate();
-//   const [form, setForm] = useState({
-//     email: "",
-//     password: "",
-//   });
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState("");
-
-//   const handleChange = (e) => {
-//     setForm({
-//       ...form,
-//       [e.target.name]: e.target.value,
-//     });
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     setError("");
-
-//     try {
-//       setLoading(true);
-
-//       const data = await loginUser(form);
-
-//       // save token + user
-//       login(data);
-
-//       toastSuccess("Login Successfull");
-
-//       // role-based redirect
-//       if (data.user.role === "admin") {
-//         navigate("/admin");
-//       } else if (data.user.role === "mentor") {
-//         navigate("/mentor");
-//       } else {
-//         navigate("/intern");
-//       }
-//     } catch (err) {
-//       setError(err.response?.data?.message || "Login failed");
-//       toastError(err.response?.data?.message || "Login failed");
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-//   return (
-//     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-//       <form
-//         onSubmit={handleSubmit}
-//         className="bg-white p-8 rounded-lg shadow-md w-96"
-//       >
-//         <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
-
-//         {error && <p className="text-red-500 mb-4 text-sm">{error}</p>}
-
-//         <input
-//           type="email"
-//           name="email"
-//           placeholder="Email"
-//           className="w-full mb-4 px-3 py-2 border rounded"
-//           onChange={handleChange}
-//           required
-//         />
-
-//         <input
-//           type="password"
-//           name="password"
-//           placeholder="Password"
-//           className="w-full mb-4 px-3 py-2 border rounded"
-//           onChange={handleChange}
-//           required
-//         />
-
-//         <button
-//           type="submit"
-//           disabled={loading}
-//           className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
-//         >
-//           {loading ? "Logging in..." : "Login"}
-//         </button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Login;
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import { loginUser } from "../../api/auth.api";
-import { Eye, EyeOff } from "lucide-react";
+import { Eye, EyeOff, X, Mail, Lock } from "lucide-react";
 import { toastError, toastSuccess } from "../../utils/toast";
-import { X } from "lucide-react";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -110,11 +17,35 @@ const Login = () => {
     password: "",
   });
 
-  const handleChange = (e) =>
+  const [errors, setErrors] = useState({});
+
+  const emailRegex =
+    /^[a-zA-Z][a-zA-Z0-9._%+-]{2,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
+    setErrors({ ...errors, [e.target.name]: "" });
+  };
+
+  const validate = () => {
+    let err = {};
+
+    if (!emailRegex.test(form.email)) {
+      err.email = `"${form.email}" is not a valid email address`;
+    }
+
+    if (!form.password) {
+      err.password = "Password is required";
+    }
+
+    setErrors(err);
+    return Object.keys(err).length === 0;
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!validate()) return;
+
     try {
       setLoading(true);
       const data = await loginUser(form);
@@ -133,44 +64,72 @@ const Login = () => {
   };
 
   return (
-    <div className=" min-h-screen flex items-center justify-center bg-linear-to-br from-indigo-100 to-blue-100">
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
+      {/* BLUR BACKGROUND */}
+      <div className="absolute inset-0 bg-linear-to-br from-blue-50 to-indigo-100 backdrop-blur-sm"></div>
+
+      {/* FORM */}
       <form
         onSubmit={handleSubmit}
-        className="relative bg-white p-8 rounded-2xl shadow-xl w-90"
+        className="relative bg-white w-full max-w-md p-8 rounded-2xl shadow-xl mx-4"
       >
+        <Link to="/" className="absolute right-3 top-3 text-gray-400">
+          <X />
+        </Link>
+
         <h2 className="text-2xl font-bold text-center mb-6">Welcome Back 👋</h2>
 
-        <input
-          name="email"
-          placeholder="Email"
-          className="w-full px-4 py-2.5 border border-gray-300 rounded-lg outline-none text-sm focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition
-   pr-10"
-          onChange={handleChange}
-          required
-        />
+        {/* EMAIL */}
+        <label className="block text-sm font-medium mb-1">Email Address</label>
 
-        <div className="relative mt-4">
+        <div className="relative">
+          <Mail className="absolute left-3 top-3 text-gray-400" size={18} />
+          <input
+            name="email"
+            placeholder="Enter your email address"
+            value={form.email}
+            onChange={handleChange}
+            className="w-full pl-10 px-4 py-2.5 border border-gray-300 rounded-lg text-sm
+            focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none"
+          />
+        </div>
+
+        {errors.email && (
+          <p className="text-red-500 font-bold text-xs mt-1">{errors.email}</p>
+        )}
+
+        {/* PASSWORD */}
+        <label className="block text-sm font-medium mt-4 mb-1">Password</label>
+
+        <div className="relative">
+          <Lock className="absolute left-3 top-3 text-gray-400" size={18} />
+
           <input
             type={show ? "text" : "password"}
             name="password"
-            placeholder="Password"
-            className="w-full px-4 py-2.5 border border-gray-300 rounded-lg outline-none text-sm focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition
-   pr-10"
+            placeholder="Enter your password"
+            value={form.password}
             onChange={handleChange}
-            required
+            className="w-full pl-10 pr-10 px-4 py-2.5 border border-gray-300 rounded-lg text-sm
+            focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none"
           />
 
           <span
             onClick={() => setShow(!show)}
             className="absolute right-3 top-3 cursor-pointer text-gray-500"
           >
-            {show ? <EyeOff /> : <Eye />}
+            {show ? <EyeOff size={18} /> : <Eye size={18} />}
           </span>
         </div>
 
+        {errors.password && (
+          <p className="text-red-500 font-bold text-xs mt-1">{errors.password}</p>
+        )}
+
         <button
           disabled={loading}
-          className="w-full mt-6 bg-indigo-600 text-white py-2 rounded-lg cursor-pointer hover:bg-indigo-700"
+          className="w-full mt-6 bg-indigo-600 hover:bg-indigo-700
+          text-white py-2.5 rounded-lg font-medium transition disabled:opacity-60 cursor-pointer"
         >
           {loading ? "Logging in..." : "Login"}
         </button>
@@ -181,12 +140,6 @@ const Login = () => {
             Register
           </Link>
         </p>
-        <span className="absolute top-2 right-2 cursor-pointer">
-          <Link to="/">
-            {" "}
-            <X />
-          </Link>
-        </span>
       </form>
     </div>
   );
--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,85 +1,195 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { User, Mail, Lock, X, Eye, EyeOff } from "lucide-react";
 import { registerUser } from "../../api/auth.api";
-import { toastSuccess, toastError } from "../../utils/toast";
-import { X } from "lucide-react";
+import { toastError, toastSuccess } from "../../utils/toast";
 
 const Register = () => {
   const navigate = useNavigate();
+  const [show, setShow] = useState(false);
+  const [showConfirm, setShowConfirm] = useState(false);
 
   const [form, setForm] = useState({
     name: "",
     email: "",
     password: "",
+    confirmPassword: "",
   });
 
-  const handleChange = (e) =>
+  const [errors, setErrors] = useState({});
+
+  const emailRegex =
+    /^[a-zA-Z][a-zA-Z0-9._%+-]{2,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
+  const fullNameRegex = /^[A-Za-z ]{3,30}$/;
+
+  const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
+    setErrors({ ...errors, [e.target.name]: "" });
+  };
+
+  const validate = () => {
+    let err = {};
+
+    if (!form.name.trim()) {
+      err.name = "Full name is required";
+    }
+
+    if(!fullNameRegex.test(form.name)){
+      err.name=`"${form.name}" can't contain special character`
+    }
+
+    if (!emailRegex.test(form.email)) {
+      err.email = `"${form.email}" is not a valid email address`;
+    }
+
+    if (!passwordRegex.test(form.password)) {
+      err.password =
+        "Password must contain 1 capital letter, 1 number, 1 special character and minimum 8 characters";
+    }
+
+    if (form.password !== form.confirmPassword) {
+      err.confirmPassword = "Passwords do not match";
+    }
+
+    setErrors(err);
+    return Object.keys(err).length === 0;
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!validate()) return;
+
     try {
       await registerUser(form);
-      toastSuccess("Registered successfully");
+      toastSuccess("Account created successfully");
       navigate("/login");
     } catch (err) {
-      toastError(err.response?.data?.message || "Register failed");
+      toastError(err.response?.data?.message || "Registration failed");
     }
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-indigo-100 to-blue-100">
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
+      {/* BLUR BACKGROUND */}
+      <div className="absolute inset-0 bg-linear-to-br from-blue-50 to-indigo-100"></div>
+
+      {/* MODAL */}
       <form
         onSubmit={handleSubmit}
-        className="relative bg-white p-8 rounded-xl shadow w-95"
+        className="relative bg-white w-full max-w-md p-8 rounded-2xl shadow-xl mx-4"
       >
+        {/* CLOSE */}
+        <Link to="/" className="absolute right-3 top-3 text-gray-400">
+          <X />
+        </Link>
+
         <h2 className="text-2xl font-bold text-center mb-6">Create Account</h2>
 
-        <input
-          name="name"
-          placeholder="Name"
-          className="w-full px-4 py-2.5 border border-gray-300 rounded-lg outline-none text-sm focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition
-   pr-10"
-          onChange={handleChange}
-        />
-        <input
-          name="email"
-          placeholder="Email"
-          className="w-full px-4 py-2.5 border border-gray-300 rounded-lg outline-none text-sm focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition
-   pr-10 mt-3"
-          onChange={handleChange}
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          className="w-full px-4 py-2.5 border border-gray-300 rounded-lg outline-none text-sm focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition
-   pr-10 mt-3"
-          onChange={handleChange}
-        />
+        {/* FULL NAME */}
+        <label className="block text-sm font-medium mb-1">Full Name</label>
 
-        {/* <select name="role" className="input mt-3" onChange={handleChange}>
-          <option value="intern">Intern</option>
-          <option value="mentor">Mentor</option>
-        </select> */}
+        <div className="relative">
+          <User className="absolute left-3 top-3 text-gray-400" size={18} />
+          <input
+            name="name"
+            placeholder="Enter your full name"
+            onChange={handleChange}
+            className="w-full pl-10 px-4 py-2.5 border border-gray-300 rounded-lg text-sm
+            focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none"
+          />
+        </div>
 
-        <button className="w-full mt-5 bg-indigo-600 text-white py-2 rounded-lg">
-          Register
+        {errors.name && (
+          <p className="text-red-500 font-bold text-xs mt-1">{errors.name}</p>
+        )}
+
+        {/* EMAIL */}
+        <label className="block text-sm font-medium mt-4 mb-1">
+          Email Address
+        </label>
+
+        <div className="relative">
+          <Mail className="absolute left-3 top-3 text-gray-400" size={18} />
+          <input
+            name="email"
+            placeholder="Enter your email address"
+            onChange={handleChange}
+            className="w-full pl-10 px-4 py-2.5 border border-gray-300 rounded-lg text-sm
+            focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none"
+          />
+        </div>
+
+        {errors.email && (
+          <p className="text-red-500 text-xs font-bold mt-1">{errors.email}</p>
+        )}
+
+        {/* PASSWORD */}
+        <label className="block text-sm font-medium mt-4 mb-1">Password</label>
+
+        <div className="relative">
+          <Lock className="absolute left-3 top-3 text-gray-400" size={18} />
+          <input
+            type={show ? "text" : "password"}
+            name="password"
+            placeholder="Create a strong password"
+            onChange={handleChange}
+            className="w-full pl-10 px-4 py-2.5 border border-gray-300 rounded-lg text-sm
+            focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none"
+          />
+          <span
+            onClick={() => setShow(!show)}
+            className="absolute right-3 top-3 cursor-pointer text-gray-500"
+          >
+            {show ? <EyeOff size={18} /> : <Eye size={18} />}
+          </span>
+        </div>
+
+        {errors.password && (
+          <p className="text-red-500 font-bold text-xs mt-1">{errors.password}</p>
+        )}
+
+        {/* CONFIRM PASSWORD */}
+        <label className="block text-sm font-medium mt-4 mb-1">
+          Confirm Password
+        </label>
+
+        <div className="relative">
+          <Lock className="absolute left-3 top-3 text-gray-400" size={18} />
+          <input
+            type={showConfirm ? "text" : "password"}
+            name="confirmPassword"
+            placeholder="Re-enter your password"
+            onChange={handleChange}
+            className="w-full pl-10 px-4 py-2.5 border border-gray-300 rounded-lg text-sm
+            focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none"
+          />
+          <span
+            onClick={() => setShowConfirm(!showConfirm)}
+            className="absolute right-3 top-3 cursor-pointer text-gray-500"
+          >
+            {showConfirm ? <EyeOff size={18} /> : <Eye size={18} />}
+          </span>
+        </div>
+
+        {errors.confirmPassword && (
+          <p className="text-red-500 font-bold text-xs mt-1">{errors.confirmPassword}</p>
+        )}
+
+        <button
+          className="w-full mt-6 bg-indigo-600 hover:bg-indigo-700
+          text-white py-2.5 rounded-lg font-medium transition cursor-pointer"
+        >
+          Create Account
         </button>
 
         <p className="text-sm text-center mt-4">
-          Already have account?{" "}
-          <Link to="/login" className="text-indigo-600">
+          Already have an account?{" "}
+          <Link to="/login" className="text-indigo-600 font-medium">
             Login
           </Link>
         </p>
-
-        <span className="absolute top-2 right-2 cursor-pointer">
-          <Link to="/">
-            {" "}
-            <X />
-          </Link>
-        </span>
       </form>
     </div>
   );
